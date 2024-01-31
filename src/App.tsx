6485bb56074556efe9fe4d8da3d70977c3df67b9
Lost Cat Dev <hinhab1@gmail.com>
2024-01-31 17:53:43 +0200

    ItzJustUpdate

--- src/App.tsx
+++ src/App.tsx
@@ -16,18 +16,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Coming soon</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Go to<code><a href='https://github.com/lost-cat-dev/lost-cat-dev-'>my profile</a></code> to see progress
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
