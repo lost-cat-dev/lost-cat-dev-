bf379974d15942bc773c752593db9aad83d4a6d8
lost-cat-dev <110544673+lost-cat-dev@users.noreply.github.com>
2024-02-01 07:03:51 +0000

    0.0.1111

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/cute-cat.png'
+
 import viteLogo from '/pic.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Coming soon</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Go to<code><a href='https://github.com/lost-cat-dev/lost-cat-dev-'>my profile</a></code> to see progress
-        </p>
+      <h1>LostCatDev</h1>
+      <p className="description">
+       <h3>Lost in real life...</h3>
+      </p>
+      <img src={viteLogo}></img>
       </div>
     </>
   )
